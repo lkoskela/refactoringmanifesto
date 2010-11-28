64e5ece267207b57c527c27a4d125c168a4ffa8b
Lasse Koskela <lasse.koskela@gmail.com>
2010-11-28 16:37:45 +0000

    Remove unnecessary printout

--- lib/manifesto.rb
+++ lib/manifesto.rb
@@ -1,7 +1,6 @@
 module Manifesto
   def self.commandments
     manifesto_source = File.join(File.dirname(__FILE__), 'manifesto.yml')
-    puts "Loading manifesto from #{manifesto_source}"
     YAML.load_file(manifesto_source)
   end
 end
