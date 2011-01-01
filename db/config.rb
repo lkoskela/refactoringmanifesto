2679ca0f31f224a22d19549cb2087d27cdcd207b
Lasse Koskela <lasse.koskela@gmail.com>
2011-01-01 20:52:45 +0000

    Add logging into db/config.rb to make it easier to figure out which environment/database stuff is run against.

--- db/config.rb
+++ db/config.rb
@@ -1,4 +1,11 @@
+def log(msg)
+    open("application.log", "a+") do |f|
+      f << "[CONFIG] " << msg << "\n"
+    end
+end
+
 configure :production do
+  log "PRODUCTION"
   DataMapper.setup(:default, "sqlite3://#{File.join(Dir.pwd, 'db/production.db')}")
 end
 
@@ -8,6 +15,7 @@ configure :test do
 end
 
 configure :development do
+  log "DEVELOPMENT"
   DataMapper.setup(:default, "sqlite3://#{File.join(Dir.pwd, 'db/development.db')}")
   DataMapper.auto_migrate!
 end
