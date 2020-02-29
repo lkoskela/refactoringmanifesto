9bdf2add1284d4f25b5238256928ef31c2180fa4
Bas Vodde <basv@odd-e.com>
2020-02-29 10:16:09 +0800

    Removed white space

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -7,30 +7,30 @@ describe "Login page" do
   def app
     @app ||= Sinatra::Application
   end
-  
+
   before(:each) do
     @admin_username = 'correct_username'
     @admin_password = 'correct_password'
     User.register(@admin_username, @admin_password)
     get "/login"
   end
-  
+
   def log_in(parameters={})
     opts = {:username => @admin_username, :password => @admin_password }
     opts.update(parameters)
     opts[:password] = SHA1.hash(opts[:password])
     post '/login', opts
   end
-  
+
   def log_out
     get '/logout'
   end
-  
+
   it "should render a login form for GET requests" do
     last_response.body.should have_xpath("//form//input[@name='username']")
     last_response.body.should have_xpath("//form//input[@name='password']")
   end
-  
+
   it "should reject authentication with missing username" do
     log_in(:username => 'nosuchuser')
     should_have_redirected_to %r{/login$}
@@ -42,13 +42,13 @@ describe "Login page" do
     should_have_redirected_to %r{/login$}
     response_body_after_redirect.should have_xpath("//*[@class='error']")
   end
-  
+
   it "should accept authentication with correct credentials" do
     log_in
     should_have_redirected_to %r{/admin$}
     response_body_after_redirect.should_not have_selector(".error")
   end
-  
+
   it "is shown after log out" do
     log_in
     should_have_redirected_to %r{/admin$}
